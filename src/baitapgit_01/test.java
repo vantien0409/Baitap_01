e3baeba72225cff3fc0ddc1a798faf27cdc9e992
vantien0409 <vantien0404@gmail.com>
2024-09-26 10:02:18 +0700

    update code of feature-test

--- src/baitapgit_01/test.java
+++ src/baitapgit_01/test.java
@@ -6,6 +6,9 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("hello 2");
+		int a =2;
+		int b=3;
+		System.out.println(a+a);
 	}
 
 }
